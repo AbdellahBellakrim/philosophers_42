6b86a7ca450d60c90310981a09f0532ccb6ba393
abdellah bellakrim <bellakrim2032@gmail.com>
2022-04-20 07:17:26 +0000

    start creating threads

--- philo/philo.c
+++ philo/philo.c
@@ -6,12 +6,38 @@
 /*   By: abellakr <abellakr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/16 02:33:24 by abellakr          #+#    #+#             */
-/*   Updated: 2022/04/16 17:45:29 by abellakr         ###   ########.fr       */
+/*   Updated: 2022/04/20 07:09:23 by abellakr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+//-----------------------------------------
+// --------------------------------------------
+void	init_philos(t_args *args)
+{
+	pthread_t th[args->number_philos];
+	int i;
 
+	i = 0;
+	while(i < args->number_philos)
+	{
+		pthread_create(&th[i], NULL, &routine, NULL);
+		i++;
+	}
+	i = 0;
+	while(i < args->number_philos)
+	{
+		pthread_join(th[i], NULL);
+		i++;
+	}
+}
+//------------------------------------------ 
+void*	routine()
+{
+	printf("test from the thread\n");
+	return 0;
+}
+//------------------------------------------ 
 int main(int ac, char **av)
 {
 	t_args *args;
@@ -22,6 +48,8 @@ int main(int ac, char **av)
 	if(!args)
 		return(write(2, "out of memory\n", 14), 1);
 	parsing(ac, av, args);
+	init_philos(args);
+	return(0);
 	// printf("number of philos : %d\n", args->number_philos);
 	// printf("die_time : %d \n", args->die_time);
 	// printf("eat_time : %d \n", args->eat_time);
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: abellakr <abellakr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/16 02:33:33 by abellakr          #+#    #+#             */
-/*   Updated: 2022/04/16 17:24:20 by abellakr         ###   ########.fr       */
+/*   Updated: 2022/04/20 07:09:49 by abellakr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #define PHILO_H
 //----------------------------------------
 #include <libc.h>
+#include <pthread.h>
 //-----------------------------------------
 typedef struct t_args 
 {
@@ -23,6 +24,13 @@ typedef struct t_args
 	int		sleep_time;
 	int		meal_number;
 }t_args;
+// ------------------------
+typedef struct t_features
+{
+	int philo_id;
+	int philo_meals;
+	
+}t_features;
 //-------------------------------------------
 int		check_arguments(int ac, char **av);
 int		ft_isdigit(int str);
@@ -31,4 +39,6 @@ void	parsing(int ac, char **av, t_args *args);
 int		ft_atoi(char *p);
 char	*ft_trizo(char *s, char c);
 int		check_max(long long number, int signe);
+void	init_philos(t_args *args);
+void*	routine();
 #endif
